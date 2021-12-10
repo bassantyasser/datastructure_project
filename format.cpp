0bfb77f37171bb7bdcb97a2393c99f5f8a22f9ca
wezza <mohamedmoatazali60@gmail.com>
2021-12-10 19:56:55 +0200

    added parse_data && data_to_file

--- format.cpp
+++ format.cpp
@@ -10,16 +10,18 @@ typedef long long ll;
 
 void indent (int n, fstream& file);
 void Print_tag (int indentation, fstream& file, string& line, int& i);
-void Print_data (int indentation, fstream& file, string& line, int& i);
+void Parse_data (int indentation, fstream& file, string& line, int& i, string& data);
 void remove_space (string& line, int& i);
+void data_to_file (int indentation, fstream& file, string& line, int& i, string& data);
 // void erase_newLine (string& str);
 
 
 int main ()
 {
     fstream my_file, temp_file;
-    my_file.open ("test.xml", ios::in);
-    temp_file.open ("temp.xml", ios::out);
+    static string data = "";
+    my_file.open ("C:\\Users\\hecker\\Desktop\\Data_Project\\project\\test.xml", ios::in);
+    temp_file.open ("C:\\Users\\hecker\\Desktop\\Data_Project\\project\\temp.xml", ios::out);
 
     if (my_file.is_open () && temp_file.is_open ())
     {
@@ -37,6 +39,7 @@ int main ()
                     {
                         if (line[i + 1] == '/') //closing
                         {
+                            data_to_file (indentation, temp_file, line, i, data);
                             indentation --;
                             //print the tag
                             Print_tag (indentation, temp_file, line, i);
@@ -49,7 +52,7 @@ int main ()
                     } else //data
                     {
                         //print data
-                        Print_data (indentation, temp_file, line, i);
+                        Parse_data (indentation, temp_file, line, i, data);
                     }
                 }
             }
@@ -63,21 +66,32 @@ int main ()
 }
 
 
-void Print_data (int indentation, fstream& file, string& line, int& i)
+void Parse_data (int indentation, fstream& file, string& line, int& i, string& data)
 {
     // remove_space (line, i);
     // if (i >= line.size ())
     //     return;
 
-    indent (indentation, file);
     while (line[i] != '<' && i < line.size ())
     {
-        file << line[i++];
+        // indent (indentation, file);
+        data += line[i++];
     }
-    file << endl;
+    if (line[i] == '<')  //data is finished
+        data_to_file (indentation, file, line, i, data);
+
+    // file << endl;
     i --;
 }
 
+void data_to_file (int indentation, fstream& file, string& line, int& i, string& data)
+{
+    indent (indentation, file);
+    file << data;
+    if (data != "")
+        file << endl;
+    data = "";
+}
 void Print_tag (int indentation, fstream& file, string& line, int& i) //tested
 {
     // if (i == line.size ())
