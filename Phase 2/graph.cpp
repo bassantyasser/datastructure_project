b0ec5ea5d8ace049e5fab7a45f46b1d73b67a1b7
wezza <mohamedmoatazali60@gmail.com>
2022-02-07 23:13:30 +0200

    added dot method

--- Phase 2/graph.cpp
+++ Phase 2/graph.cpp
@@ -1,5 +1,5 @@
 vector <vector <int>> MakeList(vector <Node*> tr)
-{   
+{
     vector <vector <int>> list;
     list.resize(10, vector <int>());
     int l = 0;
@@ -10,15 +10,15 @@ vector <vector <int>> MakeList(vector <Node*> tr)
         {
             while (i < tr.size() - 1 && tr[i + 1]->gettagname() != "<user>" )
             {
-                
+
                 if (tr[i + 1]->gettagname() == "<id>" )
                 {
-                    
+
                     list[l].push_back(stoi(tr[i + 1]->getdata()));
-                   
+
                 }
                 i++;
-                
+
             }
             l++;
         }
@@ -41,6 +41,21 @@ void graph(string s)
     xmltree(root, data, i, root);
     vector <Node*> t = preorderTraversal(root);
     vector <vector <int>> v = MakeList(t);
+    dot(v);
 
-
+}
+void dot(vector<vector<int>> list)
+{
+    fstream myfile;
+    myfile.open("a.dot", ios::out); // 8 2 3
+    myfile << "digraph test{\n";
+    for (int i = 0; i < (int)list.size(); i++)
+    {
+        for (int j = 1; j < (int)list[i].size(); j++)
+        {
+            myfile << list[i][0] << "->" << list[i][j] << endl;
+        }
+    }
+    myfile << "}";
+    myfile.close();
 }
